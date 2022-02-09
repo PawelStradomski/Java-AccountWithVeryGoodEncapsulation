77ffbe3133530b5de2075e8887c8251ca145b4d9
pawelstradomski <pawelstradomski@localhost>
2022-02-09 13:08:41 +0100

    AccountWithVeryGodEncapsulation

---
+++ src/accountwithverygoodencapsulation/AccountWithVeryGoodEncapsulation.java
@@ -0,0 +1,55 @@
+
+package accountwithverygoodencapsulation;
+
+/**
+ *
+ * @author pawelstradomski
+ */
+public class AccountWithVeryGoodEncapsulation {
+
+    public AccountWithVeryGoodEncapsulation(int accountNumber) 
+    {
+        this.accountNumber = accountNumber;
+    }
+    public int getAccountBalance()
+    {
+        return this.accountBalance;
+    }
+    public void deposit(int amountToDeposit)
+    {
+        if(amountToDeposit > 0)
+        {
+            this.accountBalance = this.accountBalance + amountToDeposit;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Kwota do wplaty - amountToDeposit musi byc wieksza od zera");
+        }
+    }
+    public void withdraw(int amountToWithdraw)
+    {
+        if(amountToWithdraw <= 0) throw new IllegalArgumentException("Kwota do wyplaty - amountToWithdraw musi byc wieksza od zera");
+        if(accountBalance - amountToWithdraw > 0)
+        {
+            this.accountBalance -= amountToWithdraw;
+        }
+        else
+        {
+            throw new RuntimeException("Nie masz pieniedzy na koncie");
+        }
+        
+    }
+    private final int accountNumber;
+    private int accountBalance;
+    
+    
+
+    
+    public static void main(String[] args) {
+        AccountWithVeryGoodEncapsulation kontoBankowe = new AccountWithVeryGoodEncapsulation(611020);
+        kontoBankowe.deposit(150);
+        kontoBankowe.withdraw(45);
+        System.out.println("Twoj stan konta" + " " +kontoBankowe.getAccountBalance());
+    }
+    
+}
